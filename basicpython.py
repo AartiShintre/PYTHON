02d44949eed9f49e01a2d09bcce4c64e76afd51b
AartiShintre <aartishintre100@gmail.com>
2025-02-24 23:34:31 +0530

    Add files via upload

---
+++ basicpython.py
@@ -0,0 +1,15 @@
+print("arti is don")
+print(5)
+print("arti",2,3,4.6)
+print("arti",2,3,4.6,sep=",")
+#bollen
+print(True)
+#complex
+print(5+6j)
+#list
+print([1,2,3,4,5])
+#tuple
+print((1,2,3,4,5))
+#sets
+print({1,2,3,4,5})
+print (type(int(4.5)))
