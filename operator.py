c2ce171b61ee21516e4cb1c0da270eb9b26dc564
AartiShintre <aartishintre100@gmail.com>
2025-02-24 23:34:52 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,30 @@
+#arthematic opretor
+print(5+2)
+print(5-2)
+print(5*2)
+print(5/2)
+print(5//2)
+print(5%2)
+print(4**2)
+#relational opretor
+print(3<4)
+print(3>4)
+print(3<=3)
+print(4==4)
+print(2!=3)
+#logical opretor
+print(1 and 1)
+print(0 or 1)
+print(not 1)
+#bitwise opretor
+print(2&3)
+print(2|3)
+print(2^3)
+print(~3)
+#assignment operator
+a=5
+print(a)
+#membership operator
+#in/not in
+print('A'in 'Arti')
+print(1 in[1,2,4,3])
