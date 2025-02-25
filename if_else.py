1f17abba069fcff7f01e2c3d105f5649d037052d
AartiShintre <aartishintre100@gmail.com>
2025-02-25 22:30:23 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,8 @@
+email=input("enter email")
+password=input("enter password")
+if email=="aartishintre100@gmail.com" and password=="Arti@2504":
+    print("welcome")
+elif email=="aartishintre100@gmail.com" and password!="Arti@2504":
+    print("password incorrect")    
+else:
+    print("wrong login")    
