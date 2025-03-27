aab80bd99356e950a87764e22c50b826eeb3801d
AartiShintre <aartishintre100@gmail.com>
2025-03-27 21:59:07 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,17 @@
+l=[1,2,3,4]
+print(l)
+print(type(l))
+
+#multiple type of data available
+l=[1,2,3,1.22,'arti']
+print(l)
+print(l[2])
+#list is mutable
+l[3]=6
+print(l)
+#duplicates are available
+l=[1,1,2,3,1]
+print(l)
+print(sorted(l,reverse=True))
+l=[2,3,4,5]
+print(l.pop())
